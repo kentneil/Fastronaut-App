9883059a60202e1b5915f286fb4798673ce777f0
kentneil <kentneilborgoniaitong17@gmail.com>
2024-11-14 09:55:07 +0800

    First Commit

---
+++ Fastronaut/app/_layout.jsx
@@ -0,0 +1,19 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text><W>WELCOME TO EARLZONE APP</W></Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
